e7a134bb3edeb10894271ef4a5ecd3a5328be43c
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-18 19:54:44 +0530

    Add files via upload

---
+++ Celcius.java
@@ -0,0 +1,10 @@
+// Celcius into Fahrenheit
+import java.lang.Math.*;
+public class Celcius{
+	public static void main(String[] args){
+		float celcius=14;
+		
+		float fah=32+((celcius*9)/5);
+		System.out.println("Celcius into Fahrenheit is: "+fah);
+	}
+}
