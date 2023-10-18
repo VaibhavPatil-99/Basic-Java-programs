238f006099940ca46e18c1ff0613ff18d7bf874d
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-18 19:55:35 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,11 @@
+//Area of circle
+import java.lang.Math.*;
+class Circle{
+	public static void main(String[] args)
+	{
+		double r=4;
+		double area = Math.PI*(r*r);
+		
+		System.out.println("The area is: "+area);
+		}
+		}
