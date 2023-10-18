6363f8cf24b4ec065a2e4e0f16ab796b33730f57
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-18 19:53:49 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,12 @@
+//Area and Perimeter of rectangle
+public class Area{
+	public static void main(String[] args){
+		int length=10;
+		int breadth=5;
+		int area=length*breadth;
+		int perimeter=2*(length+breadth);
+		
+		System.out.println("The area of rectangle is: "+area);
+		System.out.println("The perimeter of rectangle is: "+perimeter);
+		}
+	}
