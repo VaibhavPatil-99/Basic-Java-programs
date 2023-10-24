eb2ad530334d157f07fbbdcd7bb4a915573817f1
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-24 22:54:54 +0530

    Add files via upload

---
+++ Min.java
@@ -0,0 +1,20 @@
+public class Min{
+	public static void main(String[] args){
+		int a=10;
+		int b=20;
+		int c=30;
+		
+		if(a<b && a<c)
+		{
+		 System.out.println(a+ " is a mininmun number");
+		}
+		else if(b<a && b<c)
+		{
+		 System.out.println(b+ "is a minimum number");
+		}
+		else
+		{
+		 System.out.println(c+ "is a minimum number");
+		}
+		}
+		}
