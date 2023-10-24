74083150de100556cfa71130289f7456e7bc1dcb
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-24 22:57:38 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,24 @@
+public class Operator{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=10;
+		int c=10;
+		int d=10;
+		
+		//Increment
+		
+		System.out.println("post increment a: "+ (a++));
+		
+		System.out.println("pre increment a: "+ (++b));
+		
+		//Decrement
+		
+		System.out.println("post decrement c: "+ (c--));
+		
+		System.out.println("pre decrement d: "+ (--d));
+		
+	}
+}
+		
+	
