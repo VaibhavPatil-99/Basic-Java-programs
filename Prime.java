d43af24593af5455273a68895b5c9dea0043b435
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-24 22:58:40 +0530

    Add files via upload

--- Prime.java
+++ Prime.java
@@ -1,23 +1,29 @@
-package Basics;
-
-public class Prime {
-	public static void main(String[] args) {
-		int num=13;
-		int temp=0;
-		
-		for(int i=2;i<num;i++) {
-			if(num%i==0)
-			{
-				temp=1;
-				break;
-			}
+import java.util.Scanner;
+public class Prime{
+	
+	
+	public static void main(String[] args){
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter a number");
+	
+	
+     int count=0;
+	 int num= sc.nextInt();
+	 sc.close();
+	  for(int i=2;i<num;i++)
+	  {
+	   if(num%i==0){
+	   count++;
+	   break;
+	   }
+	  }
+		if (count == 0)
+		{
+			 System.out.println(num + " is prime");
 		}
-			if(temp==0) {
-				 System.out.println("prime");
-			}
-			else {
-				System.out.println("not prime");
-			}
+		else 
+		{
+			 System.out.println("not prime");
 		}
 	}
-
+}
