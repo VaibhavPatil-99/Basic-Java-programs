1e9fc6206d8b5de9a203529f355ea264c61fc52e
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-24 22:55:26 +0530

    Add files via upload

---
+++ Modulus.java
@@ -0,0 +1,10 @@
+public class Modulus{
+	public static void main(String[] args)
+	{
+		int num1=40;
+		int num2=12;
+		int result=0;
+		result=num1%num2;
+		System.out.println("The modulus of num1 & num2 is : "+result);
+	}
+}
