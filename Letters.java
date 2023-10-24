ded74b601311b365a457c0162ca454fa95af895d
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-24 22:53:01 +0530

    Add files via upload

---
+++ Letters.java
@@ -0,0 +1,16 @@
+// upperscase or lowercase
+public class Letters{
+	public static void main(String[] args)
+	{
+	 char ch = 'b';
+	 
+	 if(ch>='A' && ch<='Z')
+	 {
+	  System.out.println(ch+ "The letter is uppercase");
+	 }
+	 else
+	 {
+	  System.out.println(ch+ "The letter is lowercase");
+	 }
+	}
+}
