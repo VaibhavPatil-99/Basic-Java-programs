8f6b43e16be3ab0f676512af1b01b55cc20d8907
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-24 22:56:59 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,16 @@
+// Check the number is odd or even
+
+public class OddEven{
+	public static void main(String[] args){
+		
+		int num1=9;
+		
+		if(num1%2==0)
+		{ 
+			System.out.println("The number is even");
+		}
+		else
+			System.out.println("The number is odd");
+	}
+}
+		
