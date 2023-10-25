f4e5007550285c8591cbc58f9776aa60f5db9909
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-25 16:48:27 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,21 @@
+// swapping using Temp variable
+public class Swap{
+	public static void main(String[] args)
+	{
+		int a=100;
+		int b=55;
+		
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+		System.out.println("***************");
+		
+		
+		int temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+	}
+}
+		
