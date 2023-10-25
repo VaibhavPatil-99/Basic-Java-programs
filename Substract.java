ac02ad71e3c37ce80f1da2307e43a1f8f09cd2f6
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-25 16:46:06 +0530

    Add files via upload

---
+++ Substract.java
@@ -0,0 +1,10 @@
+class Substract{
+	public static void main(String[] args){
+		int num1=50;
+		int num2=10;
+		int result = num1-num2;
+		
+		System.out.println("The result is :"+result);
+		}
+	}
+		
