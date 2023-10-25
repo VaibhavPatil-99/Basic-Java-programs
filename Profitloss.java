7f672ae5f0963fddd42867e840121abb816c23c6
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-25 16:43:05 +0530

    Add files via upload

---
+++ Profitloss.java
@@ -0,0 +1,15 @@
+public class Profitloss{
+	public static void main(String[] args){
+		int sP=25000;
+		int cP=30000;
+		
+		if(sP>cP)
+		{
+		 System.out.println("Profit");
+		}
+		else
+		{
+		 System.out.println("Loss");
+		}
+		}
+		}
