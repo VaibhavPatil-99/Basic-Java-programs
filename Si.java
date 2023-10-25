013780fc7425343d5bfbee52e72d047e58272660
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-25 16:44:09 +0530

    Add files via upload

---
+++ Si.java
@@ -0,0 +1,11 @@
+// Simple interest
+public class Si{
+	public static void main(String[] args){
+		int p=20000;
+		int r=4;
+		int t=2;
+		
+		double si=(p*r*t)/100;
+		System.out.println("The simple interest is: "+si);
+		}
+	}
