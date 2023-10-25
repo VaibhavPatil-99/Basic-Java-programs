97fd48a09f9864c6f29ea070029729d86308eac6
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-25 16:48:56 +0530

    Add files via upload

---
+++ Swap2.java
@@ -0,0 +1,18 @@
+// swapping of two numbers without using variables
+public class Swap2{
+ public static void main(String[] args){
+	int a=100;
+	int b=55;
+	 System.out.println("a: "+a);
+	 System.out.println("b: "+b);
+	 System.out.println("***************************");
+	 
+	 a=a+b;
+	 b=a-b;
+	 a=a-b;
+	 
+	 System.out.println("a: "+a);
+	 System.out.println("b: "+b);
+	 }
+	}
+	 
