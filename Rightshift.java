180551a9fedc9afa9559b2e42db4f6b1dd2b73e5
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-25 16:43:48 +0530

    Add files via upload

---
+++ Rightshift.java
@@ -0,0 +1,18 @@
+
+public class Rightshift{
+	public static void main(String[] args){
+	
+	int a=20;
+	
+	System.out.println("left shift operationn :"+(20<<1));
+	System.out.println("Right shift operationn :"+(20>>1));
+	
+	int s=-10;
+	
+	System.out.println("unsigned right shift : "+(-10>>>1));
+	
+	System.out.println(Integer.toBinaryString(20<<1));
+	System.out.println(Integer.toBinaryString(20>>1));
+	
+	}
+}
