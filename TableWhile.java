2b4fdc4e7529c391b097b0ad66c2baa69a6eaa4f
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-26 23:28:12 +0530

    Add files via upload

---
+++ TableWhile.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TableWhile{
+	public static void main(String[] args){
+	 
+	 Scanner sc= new Scanner(System.in);
+	 System.out.println("enter a number : ");
+	 
+	 int num= sc.nextInt();
+	 int i=1;
+	 
+	 while(i<=10)
+	 {
+	  System.out.println(num+ " * " + i + " = " + (num * i));
+	  i++;
+	 }
+	 }
+	 }
